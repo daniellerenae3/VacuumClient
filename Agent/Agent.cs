b4670b5a044419afae5598a7284d7c8791e4724f
Danielle <danielle.crawford@onshoreoutsourcing.com>
2017-11-08 12:16:47 -0600

    Added switch statement

--- Agent/Agent.cs
+++ Agent/Agent.cs
@@ -12,7 +12,47 @@ namespace IntelligentVacuum.Agent
 
         public AgentAction DecideAction(Room room)
         {
-            return AgentAction.NONE;
+            AgentAction action;
+            // SENSE
+            bool isDirty = room.IsDirty;
+            //New Instantiation of Random
+            Random random = new Random();
+            int randomNum = random.Next(1, 4);
+
+            // PLAN
+            if (isDirty)
+            {
+                action = AgentAction.CLEAN;
+            }
+            else
+            {
+                switch (randomNum)
+                {
+                    case 1:
+                        action = AgentAction.MOVE_RIGHT;
+                        break;
+
+                    case 2:
+                        action = AgentAction.MOVE_DOWN;
+                        break;
+
+                    case 3:
+                        action = AgentAction.MOVE_LEFT;
+                        break;
+
+                    case 4:
+                        action = AgentAction.MOVE_UP;
+                        break;
+
+                    default:
+                        action = AgentAction.MOVE_RIGHT;
+                        break;
+                }
+
+            }
+            // ACT
+            return action;
+
         }
     }
 }
